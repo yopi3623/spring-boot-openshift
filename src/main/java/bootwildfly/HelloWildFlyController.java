cfe4d448e1602945f295ed6f276e8277089dcd53
Magnus Larsson <magnus.larsson.ml@gmail.com>
2016-10-05 09:14:27 +0200

    Update HelloWildFlyController.java

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -9,6 +9,6 @@ public class HelloWildFlyController {
 
     @RequestMapping("hello")
     public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+        return ("Hello, Magnus SpringBoot app on Wildfly, v1");
     }
 }
