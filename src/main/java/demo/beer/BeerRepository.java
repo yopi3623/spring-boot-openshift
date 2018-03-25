706f8297e568641f590411d47ae59462eed5b51b
xbarbot <xavier.barbot@accor.com>
2018-03-25 14:39:45 +0200

    spring boot 1.5 + fusion spring-boot-angular-example matt raible

---
+++ src/main/java/demo/beer/BeerRepository.java
@@ -0,0 +1,8 @@
+package demo.beer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface BeerRepository extends JpaRepository<Beer, Long> {
+}
